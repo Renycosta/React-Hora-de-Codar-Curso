58a6a035287c6c7e60aef4cb7859fa9407132c8b
Reny <reny.dev03@gmail.com>
2026-03-31 17:15:30 -0300

    3_Avançando

--- 3_Avançando/src/App.jsx
+++ 3_Avançando/src/App.jsx
@@ -19,8 +19,40 @@ import ShowUsername from './components/ShowUsername'
 // 9 - Desestruturando props
 import CarDetails from './components/CarDetails'
 
+// 11 - Renderização de listas com componente
+const cars = [
+  { id: 1, brand: "Ferrari", color: "Amarelo", km: 0},
+  { id: 2, brand: "KIA", color: "Branco", km: 2000000},
+  { id: 3, brand: "Renault", color: "Azul", km: 32000},
+]
+
+// 12 - fragments
+import Fragment from './components/Fragment'
+
+// 13 - children
+import Container from './components/Container'
+
+// 14 - Função em prop
+import ExecuteFunction from './components/ExecuteFunction'
+
+// 15 - State lift
+import Message from './components/Message'
+import ChangeMessage from './components/ChangeMessage'
+
 function App() {
   const [count, setCount] = useState(0)
+
+  // 14 - Função em prop
+  function showMessage() {
+    console.log("Evento do componente pai")
+  }
+
+  // 15 - State lift
+  const [message, setMessage] = useState("")
+
+  const handleMessage = (msg) =>{
+    setMessage(msg)
+  }
 
   return (
     <>
@@ -47,6 +79,34 @@ function App() {
 
         {/* 9 - Desestruturando props */}
         <CarDetails brand="VW" km={999} color="Vermelho"/>
+
+        {/* 10 - Reaproveitando componentes */}
+        <CarDetails brand="VW" km={12333} color="branco"/>
+        <CarDetails km={999432} color="Azul" brand="Audi"/>
+
+        {/* 11 - Renderização de lista com componente */}
+        {cars.map((car) => (
+          <CarDetails key={car.id} brand={car.brand} color={car.color} km={car.km}/>
+        ))}
+
+        {/* 12 - fragments */}
+        <Fragment />
+
+        {/* 13 - children */}
+        <Container>
+          <p>Alguma coisa</p>
+        </Container>
+        <Container>
+          <h2>Teste</h2>
+          <p>Meu container</p>
+        </Container>
+
+        {/* 14 - Função em prop */}
+        <ExecuteFunction myFunction={showMessage}/>
+
+        {/* 15 - state lift */}
+        <Message msg={message} />
+        <ChangeMessage handleMessage={handleMessage}/>
       </div>
     </>
   )
